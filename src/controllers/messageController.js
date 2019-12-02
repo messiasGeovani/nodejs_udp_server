fb4dd0db781160fe8d5b73ae38da5ce33931a9ab
messiasGeovani <messias.geovani.lima@gmail.com>
2019-12-02 20:29:20 -0300

    message storage config

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -1,8 +1,19 @@
+// file module import
+const fs = require('fs')
+
 // message controller module
 exports.messageController = {
     
     // storage method
     async store(msg, rinfo) {
         console.log(`Server got: ${msg} from ${rinfo.address}:${rinfo.port}`)
+
+        await fs.writeFile('/tmp/messages.json', msg.toString(), err => {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log('The file was saved!')
+            }
+        })
     }
 }
